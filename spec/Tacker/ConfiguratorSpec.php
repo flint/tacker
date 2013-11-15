35bedad68a727f912210d583aeb0a68e12920d29
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-11-15 09:35:59 +0100

    Configurator and Spec

--- spec/Tacker/ConfiguratorSpec.php
+++ spec/Tacker/ConfiguratorSpec.php
@@ -4,4 +4,48 @@ namespace spec\Tacker;
 
 class ConfiguratorSpec extends \PhpSpec\ObjectBehavior
 {
+    /**
+     * @param Symfony\Component\Config\Loader\LoaderInterface $loader
+     * @param Pimple $pimple
+     */
+    function let($loader, $pimple)
+    {
+        $this->beConstructedWith($loader);
+
+        // make sure that the directory used for specs are clean
+        @array_map('unlink', glob(sys_get_temp_dir() . '/tacker/*'));
+    }
+
+    function it_caches_config($loader, $pimple)
+    {
+        $this->setCacheDir(sys_get_temp_dir() . '/tacker');
+
+        $loader->load('config.json')->willReturn(array())
+            ->shouldBeCalledTimes(1);
+
+        $this->configure($pimple, 'config.json');
+        $this->configure($pimple, 'config.json');
+    }
+
+    function it_loads_config_file($loader, $pimple)
+    {
+        $loader->load('config.json')->willReturn(array('hello' => 'world'))
+            ->shouldBeCalled();
+
+        $pimple->offsetSet('hello', 'world')->shouldBeCalled();
+
+        $this->configure($pimple, 'config.json');
+    }
+
+    function it_allows_setting_debug_and_cache_dir()
+    {
+        $this->getDebug()->shouldReturn(false);
+        $this->getCacheDir()->shouldReturn(null);
+
+        $this->setDebug(true);
+        $this->setCacheDir('/path');
+
+        $this->getDebug()->shouldReturn(true);
+        $this->getCacheDir()->shouldReturn('/path');
+    }
 }
--- src/Tacker/Configurator.php
+++ src/Tacker/Configurator.php
@@ -2,6 +2,67 @@
 
 namespace Tacker;
 
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\ConfigCache;
+use Pimple;
+
+/**
+ * @package Tacker
+ */
 class Configurator
 {
+    protected $debug = false;
+    protected $cacheDir;
+    protected $loader;
+
+    public function __construct(LoaderInterface $loader)
+    {
+        $this->loader = $loader;
+    }
+
+    public function getDebug()
+    {
+        return $this->debug;
+    }
+
+    public function getCacheDir()
+    {
+        return $this->cacheDir;
+    }
+
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
+    }
+
+    public function setCacheDir($cacheDir)
+    {
+        $this->cacheDir = $cacheDir;
+    }
+
+    public function configure(Pimple $pimple, $resource)
+    {
+        $cache = new ConfigCache(sprintf('%s/%s.php', $this->cacheDir, crc32($resource)), $this->debug);
+
+        if (!$cache->isFresh()) {
+            $parameters = $this->loader->load($resource);
+        }
+
+        if ($this->cacheDir && isset($parameters)) {
+            $cache->write('<?php $parameters = ' . var_export($parameters, true) . ';', array());
+        }
+
+        if (!isset($parameters)) {
+            require (string) $cache;
+        }
+
+        $this->build($pimple, $parameters);
+    }
+
+    protected function build(Pimple $pimple, array $parameters)
+    {
+        foreach ($parameters as $k => $v) {
+            $pimple->offsetSet($k, $v);
+        }
+    }
 }
