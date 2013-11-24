8c272f4962e2c48a9a3296c18723576ec517ff89
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-11-24 16:21:50 +0100

    fix 5.3 compatibility

--- src/Tacker/functions.php
+++ src/Tacker/functions.php
@@ -4,7 +4,13 @@ function tacker_array_map_recursive(array $array, $callback) {
     $mapped = array();
 
     foreach ($array as $k => $v) {
-        $mapped[$k] = is_array($v) ? tacker_array_map_recursive($v, $callback) : $callback($v);
+        if (is_array($v)) {
+            $v = tacker_array_map_recursive($v, $callback);
+        } else {
+            $v = call_user_func($callback, $v);
+        }
+
+        $mapped[$k] = $v;
     }
 
     return $mapped;
